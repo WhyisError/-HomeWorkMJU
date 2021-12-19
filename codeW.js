fefc3d784262da0305ba0b476b5077bf036171af
WhyThis_Error <95395070+WhyisError@users.noreply.github.com>
2021-12-19 21:54:08 +0700

    Add files via upload

---
+++ codeW.js
@@ -0,0 +1,19 @@
+function open_face() {
+    window.open("https://www.facebook.com/computersciencemju/");
+}
+
+function open_mju() {
+    window.open("https://www.mju.ac.th/th/");
+}
+
+function open_manage() {
+    window.open("https://erp.mju.ac.th/");
+}
+
+function open_pay() {
+    window.open("http://www.payroll.mju.ac.th/");
+}
+
+function open_sci() {
+    window.open("https://secretary-science.mju.ac.th/wtms_index.aspx?&lang=th-TH");
+}
